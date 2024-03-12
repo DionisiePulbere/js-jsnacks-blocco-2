de75037f005a8e421e06020c3882bd9ea1ae1ca6
DionisiePulbere <dionisiepulbere@gmail.com>
2024-03-12 17:18:13 +0100

    feat: add jsnack-2

--- jsnack-2/js/script.js
+++ jsnack-2/js/script.js
@@ -1,2 +1,13 @@
 // - L’utente inserisce due parole in successione, con due prompt.
 // Il software stampa prima la parola più corta, poi la parola più lunga.
+
+// chiedere le due parole 
+const firstWord = prompt("Inserisci la prima parola");
+const secondWord = prompt("Inserisci la seconda parola");
+
+// stabilire qual'è la più corta e qaul'è la più lunga e determinarne l'ordine 
+if (firstWord.length > secondWord.length){
+    alert(`${secondWord} ${firstWord}`);
+} else {
+    alert(`${firstWord} ${secondWord}`);
+}
