7528c1c4f36e07ba814527c37c04866f9ca7f130
DionisiePulbere <dionisiepulbere@gmail.com>
2024-03-13 01:12:59 +0100

    feat: add jsnack-4

--- jsnack-4/js/script.js
+++ jsnack-4/js/script.js
@@ -1,1 +1,15 @@
 // - Chiedi un numero di 4 cifre all’utente e calcola la somma di tutte le cifre che compongono il numero.
+
+// stabilisco qaunte cifre
+const howManyNumbers = 4;
+// chiedo all'utente di fornire il numero 
+const userNumber = prompt("Inserire un numero di " + howManyNumbers + " cifre");
+// dichiaro una variabile somma 
+let sum = 0;
+// creo un ciclo for 
+for (let i = 0; i < userNumber.length ; i++){
+    // somma le cifre tra di loro traformandole da stringa a numero
+    sum += parseInt(userNumber[i]);
+}
+// stampo il risultato 
+console.log(sum);
